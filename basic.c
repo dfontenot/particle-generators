4cf9f5f6780103e274ebe451f2df23103ffc0c30
David Fontenot <fontenod@onid.oregonstate.edu>
2012-10-17 18:56:39 -0700

    uses frame independent motion now

--- basic.c
+++ basic.c
@@ -1,239 +1,29 @@
-#include "SDL.h"
 #include <math.h>
+
+#include "SDL.h"
+
+#include "types/particle.h"
+#include "types/lst.h"
 
 #define SCR_W 500
 #define SCR_H 500
 #define SCR_BPP 32
 #define LIST_START_SIZE 30
+
+//how big the width is of each starting particle
 #define PARTICLE_START_SIZE 20
-#define SIZE_DECREASE 0.99
+
+//how many pixels a second the particle decreases at
+#define SIZE_DECREASE 5
+
+//how big the width of a particle can before it gets culled
 #define PARTICLE_SIZE_THRESHOLD 2
-#define MAX_NEG_X -2
-#define MAX_X 2
-#define MAX_NEG_Y -2
-#define MAX_Y 2
 
-typedef struct {
-    double x_apparent;
-    double y_apparent;
-    double x_speed;
-    double y_speed;
-    double size;
-}particle_t;
+#define MAX_NEG_X -100 //maximum negative speed in the x direction
+#define MAX_X 100
+#define MAX_NEG_Y -100
+#define MAX_Y 100
 
-typedef struct {
-    int sz;
-    int cap;
-    void** elems;
-}lst;
-
-particle_t* new_particle(double x_apparent, double y_apparent, double x_speed, double y_speed, double size) {
-    particle_t* retval = NULL;
-    retval = (particle_t*)malloc(sizeof(particle_t));
-    if(retval == NULL) { return NULL; }
-    
-    retval->x_apparent = x_apparent;
-    retval->y_apparent = y_apparent;
-    retval->x_speed  = x_speed;
-    retval->y_speed = y_speed;
-    retval->size = size;
-    
-    return retval;
-}
-
-/*
- * LIST PROCESSESING CODE
- */
-lst* new_lst(int size) {
-    if(size <= 0) { return NULL; }
-    
-    lst* list = (lst*)malloc(sizeof(lst));
-    if(list == NULL) { return NULL; }
-    
-    list->cap = size;
-    list->sz = 0;
-    if((list->elems = malloc(size * sizeof(void*))) == NULL) {
-        return NULL;
-    }
-    else {
-        return list;
-    }
-}
-
-//loc is zero-based
-void* get_lst(lst* list, int loc) {
-    if(loc >= list->cap || loc < 0) { return NULL; }
-    else { return list->elems[loc]; }
-}
-
-//loc is zero-based
-int set_lst(lst* list, int loc, void* val) {
-    if(loc >= list->cap || loc < 0) { return -1; }
-    
-    list->elems[loc] = val;
-    
-    return 0;
-}
-
-//doubles the list (adds another layer for the heap)
-int expand_lst(lst* list) {
-    int i;
-    void** new_lst = (void**)malloc(list->cap * 2 * sizeof(void*));
-    
-    if(new_lst == NULL) { return -1; }
-    
-    for(i = 0; i < list->cap * 2; i++) {
-        if(i <= list->sz - 1) {
-            new_lst[i] = get_lst(list, i);
-        }
-        else {
-            new_lst[i] = 0;
-        }
-    }
-    
-    free(list->elems);
-    list->elems = new_lst;
-    list->cap *= 2;
-    
-    return 0;
-}
-
-//loc is zero-indexed
-int ins_lst(lst* list, void* item, int loc) {
-    int i;
-    
-    if(loc < 0 || item == NULL) { return -1; }
-    
-    //resize
-    if(list->sz == list->cap) {
-        expand_lst(list);
-    }
-    
-    //check to see if adding to the end
-    //(no need to move things around)
-    if(loc == list->sz) {
-        list->elems[list->sz] = item;
-    }
-    else {
-        for(i = list->sz - 1; i >= loc; i--) {
-            list->elems[i + 1] = list->elems[i];
-        }
-        
-        set_lst(list, loc, item);
-    }
-    
-    list->sz++;
-    return 0;
-}
-
-//add item to the end
-int push_lst(lst* list, void* item) {
-    return ins_lst(list, item, list->sz);
-}
-
-//loc is zero indexed
-int del_lst(lst* list, int loc) {
-    int i;
-    
-    if(list == NULL || loc < 0 || list->sz <= 0) { return -1; }
-    
-    //see if removing from the end
-    if(loc == list->sz - 1) {
-        list->elems[list->sz - 1] = NULL;
-    }
-    else {
-        for(i = loc; i < list->sz; i++) {
-            list->elems[i] = list->elems[i + 1];
-        }
-    }
-    
-    list->sz--;
-    return 0;
-}
-
-int pop_lst(lst* list) {
-    return del_lst(list, list->sz - 1);
-}
-
-int clear_lst(lst* list) {
-    while(list->sz > 0) {
-        if(pop_lst(list) < 0) { return -1; }
-    }
-    
-    return 0;
-}
-
-int swap_lst(lst* list, int loc1, int loc2) {
-    if(loc1 < 0 || loc2 < 0 || loc1 >= list->sz || loc2 >= list->sz) {
-        return -1;
-    }
-     
-    void* item1 = list->elems[loc1];
-    void* item2 = list->elems[loc2];
-    
-    list->elems[loc2] = item1;
-    list->elems[loc1] = item2;
-    
-    return 0;
-}
-
-int free_lst(lst* list) {
-    free(list->elems);
-    free(list);
-    
-    return 0;
-}
-
-int cleanup_nulls(lst* list) {
-    int i, j;
-    int elem_count = 0; //non-null values
-    int new_cap = 0;
-    void** new_elems;
-    
-    for(i = 0; i < list->sz; i++) {
-        if(list->elems[i] != NULL) { elem_count++; }
-    }
-    
-    new_cap = elem_count + ((1/3) * list->sz);
-    new_elems = (void**)malloc(new_cap * sizeof(void*));
-    if(new_elems == NULL) { return -1; }
-    
-    for(i = 0, j = 0; i < list->sz; i++) {
-        if(list->elems[i] != NULL) {
-            new_elems[j] = list->elems[i];
-            j++;
-        }
-    }
-    
-    free(list->elems);
-    list->elems = new_elems;
-    list->sz = elem_count;
-    list->cap = new_cap;
-    return 0;
-}
-
-int print_lst(lst* list, char* (*print_fn)(void*)) {
-    int i;
-    
-    if(list == NULL) { printf("%p\n", list); return -1; }
-    
-    printf("[");
-    for(i = 0; i < list->sz; i++) {
-        printf("%s", print_fn(list->elems[i]));
-        
-        if(i < list->sz - 1) {
-            printf(", ");
-        }
-    }
-    printf("]\n");
-    
-    return 0;
-}
-
-/*
- * END
- */
- 
 inline void free_particles(lst* particles) {
     particle_t* p;
     int i;
@@ -287,14 +77,14 @@ void move_particles(lst* particles, double elapsed) {
         }
         
         //cull particle if it gets too small
-        if(cur_particle->size * SIZE_DECREASE <= PARTICLE_SIZE_THRESHOLD) {
+        if(cur_particle->size <= PARTICLE_SIZE_THRESHOLD) {
             set_lst(particles, i, NULL);
             continue;
         }
         
-        cur_particle->x_apparent += cur_particle->x_speed;
-        cur_particle->y_apparent += cur_particle->y_speed;
-        cur_particle->size *= SIZE_DECREASE;
+        cur_particle->x_apparent += elapsed * cur_particle->x_speed;
+        cur_particle->y_apparent += elapsed * cur_particle->y_speed;
+        cur_particle->size -= elapsed * SIZE_DECREASE;
         
         //see if the particle left the screen
         if(flr(cur_particle->x_apparent) + flr(cur_particle->size) >= SCR_W || flr(cur_particle->x_apparent) <= 0 || 
