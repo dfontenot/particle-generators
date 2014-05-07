fbed0f8150feec77080d1f78f84e079c18016412
David Fontenot <fontenod@onid.oregonstate.edu>
2014-05-07 16:18:43 -0700

    added debugging information, getting more stable, still some memory errors

--- types/circlist.c
+++ types/circlist.c
@@ -26,15 +26,20 @@
 
 //Author: David Fontenot
 //Date: Sept, 2012
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h> //for memcpy and memset
 
 #include "circlist.h"
 #include "particle.h"
 
+int count_lst = 0;
+int count_empties = 0;
+
 circ_lst_t* new_circ_lst() {
     circ_lst_t* new_lst = malloc(sizeof(circ_lst_t));
-    if(new_lst == NULL) { return NULL; }
+    if(new_lst == NULL) 
+        return NULL;
     
     new_lst->head = NULL;
     new_lst->empties = NULL;
@@ -43,34 +48,83 @@ circ_lst_t* new_circ_lst() {
 }
 
 int add_circ_lst(circ_lst_t* lst, particle_t* p) {
-    if(lst == NULL) { return -1; }
+    if(lst == NULL) 
+        return -1;
     
     if(lst->head == NULL) {
         //first added particle
         
         lst->head = malloc(sizeof(node_t));
+        printf("ALLOCATING first PARTICLE %p\n", lst->head);
         lst->head->next = lst->head;
         lst->head->prev = lst->head;
+        count_lst++;
     }
     else {
+        /*
+         * empties is null when there are no 
+         * recycled nodes to claim
+         */
         if(lst->empties != NULL) {
-            //reuse the first already malloc'd node
-            node_t* old_next = lst->empties->next;
-            old_next->prev = lst->empties->prev;
+            //reclaim an item from empties and move it over to the list
+            printf("reusing old particle\n");
+            printf("number in list (being printed): %d, number in free list: %d\n", count_lst, count_empties);
+            /* node_t* cur = lst->head->next; */
+            /* printf("list item: %p\n", lst->head); */
+
+            /* while(cur != lst->head) { */
+            /*     printf("list item: %p\n", lst->head); */
+            /*     cur = cur->next; */
+            /* } */
+
+            printf("no loop detected in lst\n");
+
+            //check if last entry to remove
+            if(lst->empties->next == lst->empties) {
+                printf("last entry in empties\n");
+                //make the head point to NULL to signal all the empties have been claimed
+
+                lst->empties->next = lst->head;
+                lst->empties->prev = lst->head->prev;
+                lst->head->prev = lst->empties;
+
+                lst->head = lst->empties;
+                lst->empties = NULL; //set empties to null
+            }
+            else {
+                //take an entry off the head of the list
+
+                printf("at least 2 entries in empties\n");
+
+                node_t* old_empties = lst->empties; //will be removing the head from the empties list
+
+                lst->empties->prev->next = lst->empties->next; //pave over element
+                lst->empties->next->prev = lst->empties->prev;
+                lst->empties = lst->empties->next; //start empties list at next element
+
+                old_empties->prev = lst->head->prev;
+                old_empties->next = lst->head;
+                lst->head->prev = old_empties;
+
+                lst->head = old_empties; //head now points to this re-used node
+            }
             
-            lst->empties->next = lst->head;
-            lst->empties->prev = lst->head->prev;
-            lst->head->prev = lst->empties;
-            lst->empties = old_next;
+            count_lst++;
+            count_empties--;
         }
         else {
-            //malloc a new node
+            //allocate a new node at the head
             
             node_t* new_node = malloc(sizeof(node_t));
-            node_t* old_last = lst->head->prev;
-            old_last->next = new_node;
-            new_node->prev = old_last;
+            printf("ALLOCATING PARTICLE %p\n", new_node);
+            new_node->prev = lst->head->prev;
             new_node->next = lst->head;
+            lst->head->prev->next = new_node;
+            lst->head->prev = new_node;
+
+            lst->head = new_node;
+
+            count_lst++;
         }
     }
     
@@ -81,7 +135,8 @@ int add_circ_lst(circ_lst_t* lst, particle_t* p) {
 }
 
 int del_circ_list(circ_lst_t* lst, node_t* to_rm) {
-    if(to_rm == NULL || lst == NULL) { return -1; }
+    if(to_rm == NULL || lst == NULL) 
+        return -1;
     
     //pave over this node in the list of active particles
     to_rm->prev->next = to_rm->next;
@@ -90,7 +145,7 @@ int del_circ_list(circ_lst_t* lst, node_t* to_rm) {
     //clear the particle data
     memset(&to_rm->p, 0, sizeof(particle_t));
     
-    //put this node at the end of the empties list
+    //put this node at the start of the empties list
     
     //check if the empties list has been initialized yet
     if(lst->empties == NULL) {
@@ -99,32 +154,50 @@ int del_circ_list(circ_lst_t* lst, node_t* to_rm) {
         lst->empties->prev = lst->empties;
     }
     else {
-        node_t* old_last = lst->empties->prev;
-        to_rm->prev = old_last;
+        //prepend to the head
         to_rm->next = lst->empties;
-        old_last->next = to_rm;
+        to_rm->prev = lst->empties->prev;
+        lst->empties->prev->next = to_rm;
+        lst->empties->prev = to_rm;
+
+        lst->empties = to_rm;
     }
+
+    count_lst--;
+    count_empties++;
     
     return 0;
 }
 
 int clear_circ_list(circ_lst_t* lst) {
-    if(lst == NULL || lst->head == NULL) { return -1; }
+    if(lst == NULL || lst->head == NULL) 
+        return -1;
 
-    node_t* cur_node = lst->head;
-    while(cur_node->next != NULL) {
+    node_t* cur_node = lst->head->next;
+    node_t* tmp;
+    printf("FREEING HEAD %p\n", cur_node->prev);
+    free(cur_node->prev); //free the head
+    while(cur_node != lst->head) {
+        cur_node = tmp;
         cur_node = cur_node->next;
-        free(cur_node->prev);
+        printf("FREEING PARTICLE %p\n", tmp);
+        free(tmp->prev);
     }
+
+    printf("PARTICLES COMPLETE\n");
     
-    if(lst->empties == NULL) { return 0; } //no empty nodes to delete
-    cur_node = lst->empties;
-    while(cur_node->next != NULL) {
+    if(lst->empties == NULL) 
+        return 0;
+
+    cur_node = lst->empties->next;
+    printf("FREEING HEAD OF EMPTIES %p", cur_node->prev);
+    free(cur_node->prev);
+    while(cur_node != NULL) {
+        cur_node = tmp;
         cur_node = cur_node->next;
-        free(cur_node->prev);
+        printf("FREEING PARTICLE %p\n", tmp);
+        free(tmp->prev);
     }
-    
-    free(lst);
-    
+
     return 0;
 }
